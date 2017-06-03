a560e05a2fe69e02c07e4a762ab6bc42f26a3a63
Tom <tom@innovativetom.com>
2017-06-03 16:11:36 -0400

    Mid Day

--- AutoBathroomFan/AutoBathroomFan.ino
+++ AutoBathroomFan/AutoBathroomFan.ino
@@ -6,6 +6,10 @@
 #define DHTTYPE DHT11   // DHT Shield uses DHT 11
 #define DHTPIN D4       // DHT Shield uses pin D4
 
+const int relayPin = D1; // Relay Shield use pin D1
+int relayState = LOW; // Start with the relay off
+const long intervalRelay = 30000;  // pause for two seconds
+
 // Existing WiFi network
 const char* ssid     = "THE WIFI";
 const char* password = "demiandthomas";
@@ -27,7 +31,7 @@ const long interval = 2000;                  // Wait this long until reading aga
 
 //Sets up the initial page 
 void handle_root() {
-  server.send(200, "text/plain", "WeMos DHT Server. Get /temp or /humidity");
+  server.send(200, "text/plain", "Welcome to the bathroom server.");
   delay(100);
 }
 
@@ -65,17 +69,71 @@ void read_sensor() {
     Serial.print(str_temperature);
     Serial.println(" °C");
     Serial.print("Temperature: ");
-    Serial.print(str_temperature*1.8+32);
+    Serial.print(temperature*1.8+32);
     Serial.println(" °F");
   }
 }
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  
+  pinMode(relayPin, OUTPUT); // Set the relay output up
+ 
+  dht.begin();
 
+  Serial.println("WeMos DHT Server");
+  Serial.println("");
+
+  // Connect to your WiFi network
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting");
+
+  // Wait for successful connection
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to: ");
+  Serial.println(ssid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+  Serial.println("");
+
+  // Initial read
+  read_sensor();
+
+  // Handle http requests
+  server.on("/", handle_root);
+/*
+  server.on("/temp", [](){
+    read_sensor();
+    char response[50];
+    snprintf(response, 50, "Temperature: %s °C", str_temperature);
+    server.send(200, "text/plain", response);
+  });
+
+  server.on("/humidity", [](){
+    read_sensor();
+    char response[50];
+    
+    snprintf(response, 50, "Humidity: %s %", str_humidity);
+    server.send(200, "text/plain", response);
+  });
+*/
+  // Start the web server
+  server.begin();
+  Serial.println("HTTP server started");
 }
+
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    
+    read_sensor();
+    char response[50];
+    snprintf(response, 50, "Humidity: %s %", str_humidity);
+    server.send ( 200, "text/plain", response);  
+    digitalWrite(relayPin, relayState);
+    
+  }
 
-}
